9a4d587df5af3f1513b2e7b7dc7c0cb6c72e506e
lab806 <cm99585@hotmail.com>
2016-10-21 08:23:02 +0800

    fix gen_ir

--- gen_ir.sh
+++ gen_ir.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-fullfile=$1;
-filename="${fullfile%.*}";
-dot_dir="./dot_code"
+dot_dir="./dot"
+pdf_dir="./pdf"
+full_file=$1;
+file_name="${full_file%.*}";
+dot_file="$dot_dir/$file_name.dot";
+pdf_file="$pdf_dir/$file_name.pdf";
+echo "test: $dot_file"
 clang -S -emit-llvm -target mips-none-none "$1" -o tmp.txt; 
 opt -O3 -instcombine -unroll-threshold=999999 tmp.txt -S -o tmp1.txt; 
 sed 's/nocapture readonly //g' tmp1.txt > tmp2.txt;
 graph-llvm-ir tmp2.txt;
-echo "digraph G {" > $filename.dot;
-echo "compound=true" >> $filename.dot;
-grep -v "red\|\"_\|^\"arrayidx\|^\"i\|^\"output" tmp.dot | grep "^\"sum\|^\"sub\|^\"add\|^\"mul\|^\"sh\|^\"t" >> $filename.dot;
-echo "}" >> $filename.dot;
-./report.sh $filename.dot;
-dot $filename.dot -Tpdf -o $filename.pdf;
+echo "digraph G {" > $dot_file;
+echo "compound=true" >> $dot_file;
+grep -v "red\|\"_\|^\"arrayidx\|^\"i\|^\"output" tmp.dot | grep "^\"sum\|^\"sub\|^\"add\|^\"mul\|^\"sh\|^\"t" >> $dot_file;
+echo "}" >> $dot_file;
+./report.sh $dot_file;
+dot $dot_file -Tpdf -o $pdf_file;
 rm tmp*;
-./parser.pl $filename.dot
-mv $filename.dot $dot_dir/
+./parser.pl $dot_file
 
 
 
